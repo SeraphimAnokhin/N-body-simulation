24b63758166bee64300a0e42849391fec6e1d14d
SeraphimAnokhin <138584469+SeraphimAnokhin@users.noreply.github.com>
2026-05-31 23:32:45 +0300

    Create model.cpp

---
+++ src/model.cpp
@@ -0,0 +1,134 @@
+#include "model.h"
+
+Model::Model(): n_bodies(),
+                time_step(),
+                time_lim(),
+                state_save_step(),
+                n_threads(),
+                m(), r0(), v0(),
+                states(), calc_time(), energy(), angular_momentum(),
+                I(), P() {}
+
+Model::~Model() {
+    if (I) {
+        delete I;
+        I = nullptr;
+    }
+    if (P) {
+        delete P;
+        P = nullptr;
+    }
+}
+
+void Model::set_integrator_type(Integrator_type type) {
+    if (I) {
+        delete I;
+        I = nullptr;
+    }
+
+    switch (type) {
+    case Integrator_type::euler:
+        I = new Euler;
+        break;
+    case Integrator_type::leapfrog:
+        I = new Leapfrog;
+        break;
+    case Integrator_type::rk4:
+        I = new RK4;
+        break;
+    case Integrator_type::adaptive_rk4:
+        I = new Adaptive_RK4;
+        break;
+    default:
+        break;
+    }
+}
+
+void Model::set_potential_type(Potential_type type) {
+    if (P) {
+        delete P;
+        P = nullptr;
+    }
+
+    switch (type) {
+    case Potential_type::newton:
+        P = new Newton;
+        break;
+    default:
+        break;
+    }
+}
+
+void Model::set_potential_params(std::vector<double> params) {
+    if (P == nullptr) {
+        throw Incorrect_input("Set potential type!");
+    }
+    try {
+        P->set_params(params);
+    }
+    catch (...) {
+        throw;
+    }
+}
+
+void Model::start_calculation() {
+    if (P == nullptr) {
+        throw Incorrect_input("Set potential type!");
+    }
+    if (I == nullptr) {
+        throw Incorrect_input("Set integrator type!");
+    }
+
+    int n_steps = int(time_lim / time_step);
+    int n_saves = int(time_lim / state_save_step);
+    int save_step = int(state_save_step / time_step);
+
+    std::vector<vec3d> curr_state(2 * n_bodies);
+    std::vector<vec3d> state_for_save(n_bodies);
+
+    if (r0.size() < n_bodies) {
+        throw Incorrect_input("Set initial positions!");
+    }
+    if (v0.size() < n_bodies) {
+        throw Incorrect_input("Set initial velocities!");
+    }
+
+    for (int i = 0; i < n_bodies; i++) {
+        curr_state[i] = r0[i];
+        curr_state[n_bodies + i] = v0[i];
+    }
+
+    double curr_energy;
+    bool calc_energy = false;;
+
+    for (int i = 0; i < n_steps; i++) {
+        if (i % save_step == 0) {
+            for (int j = 0; j < n_bodies; j++) {
+                state_for_save[j] = curr_state[j];
+            }
+            states.push_back(state_for_save);
+            angular_momentum.push_back(calc_angular_momentum(curr_state));
+            calc_energy = true;
+        }
+
+        try {
+            curr_state = I->integration_step(m, curr_state, time_step, n_threads, P, calc_energy, &curr_energy);
+        }
+        catch (...) {
+            throw;
+        }
+
+        if (i % save_step == 0) {
+            energy.push_back(curr_energy);
+            calc_energy = false;;
+        }
+    }
+}
+
+vec3d Model::calc_angular_momentum(std::vector<vec3d> state) {
+    vec3d ans(0, 0, 0);
+    for (int i = 0; i < n_bodies; i++) {
+        ans += m[i] * state[i] ^ state[n_bodies + i];
+    }
+    return ans;
+}
