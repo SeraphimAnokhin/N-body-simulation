41d344128d7b42cd33d7b8bf08ef83a8b342f255
SeraphimAnokhin <138584469+SeraphimAnokhin@users.noreply.github.com>
2026-06-06 13:06:49 +0300

    Update integrator.cpp

--- src/integrator.cpp
+++ src/integrator.cpp
@@ -8,9 +8,14 @@ std::vector<vec3d> Euler::integration_step(const std::vector<double> &m, const s
         throw Incorrect_input("Arrays size mismatch!");
     }
     std::vector<vec3d> res(2 * n_bodies);
-
-    std::vector<vec3d> accelerations = A.calc_accelerations(m, state, n_threads, P, calc_energy, energy);
-    
+    std::vector<vec3d> accelerations;
+    try {
+        accelerations = A.calc_accelerations(m, state, n_threads, P, calc_energy, energy);
+    }
+    catch (...) {
+        throw;
+    }
+        
     for (int i = 0; i < n_bodies; i++) {
         res[i] = state[i] + time_step * state[n_bodies + i];
         res[n_bodies + i] = state[n_bodies + i] + time_step * accelerations[i];
@@ -21,17 +26,101 @@ std::vector<vec3d> Euler::integration_step(const std::vector<double> &m, const s
 
 std::vector<vec3d> Leapfrog::integration_step(const std::vector<double> &m, const std::vector<vec3d> &state, double time_step, int n_threads, Potential *P,
                                               bool calc_energy, double *energy) {
-    return std::vector<vec3d>();
+                        
+    int n_bodies = m.size();
+    if (state.size() != 2 * n_bodies) {
+        throw Incorrect_input("Arrays size mismatch!");
+    }
+    std::vector<vec3d> res(2 * n_bodies);
+    std::vector<vec3d> accelerations;
+    try {
+        accelerations = A.calc_accelerations(m, state, n_threads, P, calc_energy, energy);
+    }
+    catch (...) {
+        throw;
+    }
+
+    vec3d v;
+    
+    for (int i = 0; i < n_bodies; i++) {
+        v = state[n_bodies + i] + 0.5 * time_step * accelerations[i];
+        res[i] = state[i] + time_step * v;
+        res[n_bodies + i] = v + 0.5 * time_step * accelerations[i];
+    }
+
+    return res;
 }
 
 std::vector<vec3d> RK4::integration_step(const std::vector<double> &m, const std::vector<vec3d> &state, double time_step, int n_threads, Potential *P,
                                          bool calc_energy, double *energy) {
-    return std::vector<vec3d>();
-}
+    
+    int n_bodies = m.size();
+    if (state.size() != 2 * n_bodies) {
+        throw Incorrect_input("Arrays size mismatch!");
+    }
 
-std::vector<vec3d> Adaptive_RK4::integration_step(const std::vector<double> &m, const std::vector<vec3d> &state, double time_step, int n_threads, Potential *P,
-                                                  bool calc_energy, double *energy) {
-    return std::vector<vec3d>();
+    std::vector<vec3d> k1(2 * n_bodies), k2(2 * n_bodies), k3(2 * n_bodies), k4(2 * n_bodies),
+                        state2(2 * n_bodies), state3(2 * n_bodies), state4(2 * n_bodies), res(2 * n_bodies);
+
+
+    std::vector<vec3d> a;
+    try {
+        a = A.calc_accelerations(m, state, n_threads, P, calc_energy, energy);
+    }
+    catch (...) {
+        throw;
+    }
+    for (int i = 0; i < n_bodies; i++) {
+        k1[i] = state[n_bodies + i];
+        k1[n_bodies + i] = a[i];
+    }
+
+    for (int i = 0; i < 2 * n_bodies; i++) {
+        state2[i] = state[i] + 0.5 * time_step * k1[i];
+    }
+    try {
+        a = A.calc_accelerations(m, state, n_threads, P, calc_energy, energy);
+    }
+    catch (...) {
+        throw;
+    }
+    for (int i = 0; i < n_bodies; i++) {
+        k2[i] = state2[n_bodies + i];
+        k2[n_bodies + i] = a[i];
+    }
+
+    for (int i = 0; i < 2 * n_bodies; i++) {
+        state3[i] = state[i] + 0.5 * time_step * k2[i];
+    }
+    try {
+        a = A.calc_accelerations(m, state, n_threads, P, calc_energy, energy);
+    }
+    catch (...) {
+        throw;
+    }
+    for (int i = 0; i < n_bodies; i++) {
+        k3[i] = state3[n_bodies + i];
+        k3[n_bodies + i] = a[i];
+    }
+
+    for (int i = 0; i < 2 * n_bodies; i++) {
+        state4[i] = state[i] + time_step * k3[i];
+    }
+    try {
+        a = A.calc_accelerations(m, state, n_threads, P, calc_energy, energy);
+    }
+    catch (...) {
+        throw;
+    }
+    for (int i = 0; i < n_bodies; i++) {
+        k4[i] = state4[n_bodies + i];
+        k4[n_bodies + i] = a[i];
+    }
+
+    for (int i = 0; i < 2 * n_bodies; i++) {
+        res[i] = state[i] + (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) * time_step / 6;
+    }
+    return res;
 }
 
 
@@ -65,47 +154,61 @@ void Accelerometer::calc_chunk(const std::vector<double> &m, const std::vector<v
         *energy = 0;
 
         for (int i = idx_start; i < idx_finish; i++) {
-            for (int j = 0; j < idx_start; j++) {
+            for (int j = 0; j < i; j++) {
                 *energy += P->energy(m[i], state[i], m[j], state[j]);
             }
-            for (int j = i + 1; j < idx_finish; j++) {
-                *energy += 2 * P->energy(m[i], state[i], m[j], state[j]);   // energy of 1 in field of 2 is equal to energy of 2 in field of 1
-            }
-            for (int j = idx_finish; j < n_bodies; j++) {
+            for (int j = i + 1; j < n_bodies; j++) {
                 *energy += P->energy(m[i], state[i], m[j], state[j]);
             }
-    
-            *energy /= 2;  // check later
+        }
+
+        for (int i = idx_start; i < idx_finish; i++) {
+            *energy += 0.5 * m[i] * state[n_bodies + i] * state[n_bodies + i];
         }
     }
 }
 
 std::vector<vec3d> Accelerometer::calc_accelerations(const std::vector<double> &m, const std::vector<vec3d> &state, int n_threads,
-                                                     Potential *P, bool calc_energy, double *energy)
-{
+                                                     Potential *P, bool calc_energy, double *energy) {
 
     int n_bodies = m.size();
     if (state.size() != 2 * n_bodies) {
         throw Incorrect_input("Arrays size mismatch!");
     }
 
-    std::vector<std::thread> threads(n_threads);
+    std::thread threads[n_threads];
     int step = n_bodies / n_threads;
 
     double energies[n_threads];
     std::vector<vec3d> res(n_bodies);
 
+
+    std::packaged_task<void(void)> tasks[n_threads];
+    std::future<void> futures[n_threads];
+    
     for (int i = 0; i < n_threads - 1; i++) {
-        threads[i] = std::thread([=, &res, &energies] () {this->calc_chunk(m, state, P, i * step, (i + 1) * step, res, calc_energy, energies + i);});
-        //threads[i] = std::thread(&Accelerometer::calc_chunk, m, state, P, i * step, (i + 1) * step, res, calc_energy, energies + i);
+        tasks[i] = std::packaged_task<void(void)>([=, &res, &energies] () {this->calc_chunk(m, state, P, i * step, (i + 1) * step, res, calc_energy, energies + i);});
+        futures[i] = tasks[i].get_future();
+        threads[i] = std::thread(std::move(tasks[i]));
+        //threads.push_back(std::thread([=, &res, &energies] () {this->calc_chunk(m, state, P, i * step, (i + 1) * step, res, calc_energy, energies + i);}));
     }
-    threads[n_threads - 1] = std::thread([=, &res, &energies] () {this->calc_chunk(m, state, P, (n_threads - 2) * step, n_bodies, res, calc_energy, energies + n_threads - 1);});
-    //threads[n_threads - 1] = std::thread(&Accelerometer::calc_chunk, m, state, P, (n_threads - 2) * step, n_bodies, res, calc_energy,
-    //                                     energies + n_threads - 1);
+    tasks[n_threads - 1] = std::packaged_task<void(void)>([=, &res, &energies] () {this->calc_chunk(m, state, P, (n_threads - 1) * step, n_bodies, res, calc_energy, energies + n_threads - 1);});
+    futures[n_threads - 1] = tasks[n_threads - 1].get_future();
+    threads[n_threads - 1] = std::thread(std::move(tasks[n_threads - 1]));
+    //threads.push_back(std::thread([=, &res, &energies] () {this->calc_chunk(m, state, P, (n_threads - 1) * step, n_bodies, res, calc_energy, energies + n_threads - 1);}));
 
     for (int i = 0; i < n_threads; i++) {
         threads[i].join();
     }
+    for (int i = 0; i < n_threads; i++) {
+        try {
+            futures[i].get();
+        }
+        catch (...) {
+            throw;
+        }
+    }
+
 
     if (calc_energy) {
         *energy = 0;
