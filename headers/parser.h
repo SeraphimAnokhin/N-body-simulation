33d7b097462294af579369fbb63d333514c3574b
SeraphimAnokhin <138584469+SeraphimAnokhin@users.noreply.github.com>
2026-06-06 13:04:09 +0300

    Update parser.h

--- headers/parser.h
+++ headers/parser.h
@@ -4,13 +4,15 @@
 
 #include <fstream>
 #include <vector>
+#include <string>
 #include "vec3d.h"
 #include "error.h"
 
 
 class Parser {
 public:
-    static void parse(const char *filename, std::vector<double> &m, std::vector<vec3d> &r0, std::vector<vec3d> &v0);
+    static void parse(std::string filename, std::vector<double> &m, std::vector<vec3d> &r0, std::vector<vec3d> &v0);
+    static void save(std::string filename, std::vector<std::vector<vec3d>> states);
 };
 
 
